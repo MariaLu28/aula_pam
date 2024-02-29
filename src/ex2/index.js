466d3e82f09b06678f20025055738505f100d582
MariaLu28 <malu_s2811@hotmail.com>
2024-02-28 22:42:30 -0300

    Aula 28-02

---
+++ src/ex2/index.js
@@ -0,0 +1,12 @@
+import { View, Text} from 'react-native';
+
+export default function Mensagem () {
+    return (
+        <View> 
+        <Text> Hello React-Native</Text>
+        </View>
+
+
+    );
+  
+    }
