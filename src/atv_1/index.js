ce76d8eb6645fd678f5780de5628b0eb8534736b
MariaLu28 <malu_s2811@hotmail.com>
2024-03-11 19:43:26 -0300

    Aula PAM I
    Atividade 1 pronta

--- src/atv_1/index.js
+++ src/atv_1/index.js
@@ -1,16 +1,32 @@
-import { View, Text } from 'react-native'; 
+import { View, Text, Image } from 'react-native'; 
 
 import styles from './styles';
 
-//import logo from '.../assents/002-1-react-native-png'
+import logo from '../../assets/002-1-react-native.png';
 
 export default function Atividade1() { 
 
     return(
         <View style={styles.container}>
-           //<Image source={logo} style></Image>
+           <Image source={logo} style={{width: '100%', height: '15%'}}   />
             <Text style={styles.paragraph}>
               Exemplo 2
+            </Text>
+
+            <Text style={{textAlign: 'center'}}>
+                Olá Mario!
+            </Text>
+
+            <Text style={{textAlign: 'center'}}>
+                Olá Maria!
+            </Text>
+
+            <Text style={{textAlign: 'center'}}>
+                Olá Bruna!
+            </Text>
+
+            <Text style={{textAlign: 'center'}}>
+            Olá Bruno!
             </Text>
         </View>
     );
