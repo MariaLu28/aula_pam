1f8d75d1f94a2dfab916a913a2b9bc033cb3e092
MariaLu28 <malu_s2811@hotmail.com>
2024-03-27 22:14:35 -0300

    Atividade 3
    Projeto completo!

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 //import { } from 'expo-constants';
 
 //import Mensagem from './src/ex2';
-import Exemplo_3 from './src/exemplo_3';
+//import Exemplo_3 from './src/exemplo_3';
+
+import Atv_3 from './src/atv_3'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo_3 />
-      <StatusBar style="auto" />
+      <Atv_3/>
+      <StatusBar style='auto'></StatusBar>
     </View>
   );
 }
@@ -18,11 +21,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    //paddingTop: constants.StatusBarHeight,
-    //backgroundColor: '#ff9c4a',
     backgroundColor: 'orange',
     padding: 18,
-    alignItems: 'center',
+   //alignItems: 'center',
    
   },
 });
