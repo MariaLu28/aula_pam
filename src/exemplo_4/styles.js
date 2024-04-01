967f6ffdff2f4d28efbb3792fcb412aa993865a3
MariaLu28 <malu_s2811@hotmail.com>
2024-04-01 20:39:53 -0300

    Exemplo 4
    Add const, botao
    + styles

--- src/exemplo_4/styles.js
+++ src/exemplo_4/styles.js
@@ -32,10 +32,32 @@ input: {
     borderColor: '#b71c1c',
     borderRadius: 20,
     padding: 5,
-
-
+},
+linha: {
+    borderBottomWidth: 3,
+    borderColor: '#b71c1c',
+    marginVertical: 20,
+    width: '100%'
+},
+botao: {
+    width: '70%',
+    padding: 16,
+    alignItems: 'center',
+    borderRadius: 20,
+    backgroundColor: '#b71c1c',
+    marginVertical: 20,
+},
+botaoPress: {
+    backgroundColor: '#fff',
+},
+txtBotao: {
+    color: '#ffcdd2',
+    fontSize: 20,
 
 },
+txtBotaoPress: {
+    color: '#b71c1c'
+}
 });
 
 export default styles;
