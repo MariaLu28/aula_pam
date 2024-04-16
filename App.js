6a257dcb82fe16b1b88d5143705e8e5c696da74f
MariaLu28 <malu_s2811@hotmail.com>
2024-04-15 21:26:18 -0300

    Exemplo 5
    Finalizado

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     //backgroundColor: 'orange',
     padding: 18,
-   //alignItems: 'center',
+   alignItems: 'center',
    
   },
 });
--- src/exemplo_5/styles.js
+++ src/exemplo_5/styles.js
@@ -2,66 +2,59 @@ import {StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#ff80ab',
-        paddinh: 8,
-        width: '100%',
-        borderRadius: 20,
-
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#FF80AB',
+    padding: 8,
+    width: '100%',
+    borderRadius: 20,
     },
     titulo: {
-        margin:6,
-        fontSize: 30,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: '#c51162',
-
+    margin: 6,
+    fontSize: 30,
+    fontweight: 'bold',
+    textAlign: 'center',
+    color: '#C51162',
     },
     txtSaida: {
-        margin: 6,
-        fontSize: 30,
-        fontWeight: 'bold',
-        textAlign:'center',
-        color: '#e91e63',
-
+    margin: 6,
+    fontSize: 22,
+    fontweight: 'bold',
+    textAlign: 'center',
+    color: '#E91E63',
     },
     txtEntrada: {
-        borderWidth: 4,
-        textAlign: 'center',
-        fontSize: 22,
-        //height: 50
-        borderColor: '#e91e63',
-        marginTop: 10,
-        padding: 10,
-
+    borderWidth: 4,
+    textAlign: 'center',
+    FontSize: 22,
+    borderColor: '#E91E63',
+    // height: 50,
+    color: '#E53935',
+    borderRadius: 10,
+    marginTop: 10,
+    padding: 10
     },
     button: {
-        backgroundColor: '#e91e63',
+        backgroundColor: '#E91E63',
         height: 60,
         justifyContent: 'center',
         borderRadius: 10,
         marginTop: 20,
-        width: 20,
-
-    },
-    buttonTouch: {
-        backgroundColor: '#c51162',
-
-    },
-    textButton: {
+        width: '100%',
+        },
+        buttonTouch: {
+        backgroundColor: '#C51162',
+        },
+        textButton: {
         fontSize: 30,
-        color: '#ff80ab',
+        color: '#FF80AB',
         textAlign: 'center',
-
-    },
-    textLabel: {
+        },
+        textLabel:{
         fontSize: 16,
-        fontWeight: 'bold',
-        color: '#c51162',
-
-    },
-
-});
-
-export default styles
+        fontweight: 'bold',
+        color: '#C51162',
+        },
+        });
+        
+        export default styles;
